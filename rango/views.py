9aa07ff00ea4dc4b9b68fc7295ce5f148bb67228
Scott Johnston <2323166j@student.gla.ac.uk>
2019-01-24 13:59:14 +0000

    test error correction

--- rango/views.py
+++ rango/views.py
@@ -8,5 +8,9 @@ def index(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    #return return render(request, 'rango/about.html', context=context_dict)
+    return render (request, 'rango/about.html')
 
